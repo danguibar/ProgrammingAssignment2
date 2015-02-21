53e7d33288d8cd91245dccefc555fc6c11079c1c
Dangui <daniel.g.barreto@gmail.com>
2015-02-21 01:47:08 -0500

    First commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,24 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## functions do
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+   
+   currentMat <- x
+   inverseMat <- NULL
+   
+   list(
+   setInverse = function (y) inverseMat<<- y
+   ,
+   getInverse = function()  inverseMat
+   ,
+   getMatrix = function()   currentMat
+   ,
+   setMatrix =  function(y) currentMat <<- y
+   )
+   
 }
 
 
@@ -12,4 +26,36 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        #get current values from cache
+        cacheMat <- x$getMatrix()
+        cacheInv <- x$getInverse()
+        
+        
+        if ( !identical(x,cacheMat) ){ # if matrix changed
+          
+           x$setMatrix(x)
+           x$setInverse(solve(cacheMat))
+           
+           x$getInverse()         
+        }
+        else { # if matrix didnt changed
+          if ( is.null(cacheInv) ){ # first time finds inverse not set yet
+            
+            x$setInverse(solve(cacheMat))
+            
+            x$getInverse()
+            
+          }
+          else { # on re-run dont need to do solve again, just get cached inverse
+            
+            x$getInverse()
+          }
+        }        
 }
+
+c=matrix(c(2, -1/8, -1/8, 2), nrow=2,ncol=2)
+objmatfun <- makeCacheMatrix(c)
+a<-cacheSolve(objmatfun)
+
+
